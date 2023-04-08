86b94255566626c64fff780c6fd06c77c576d048
Mohit <rnippanikar7@gmail.com>
2023-04-08 07:16:06 +0530

    serverl url for deploy 1

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,2 +1,2 @@
-export const serverUrl = "http://localhost:3002"
-//export const serverUrl = "https://kitab-s7zy.vercel.app"
+//export const serverUrl = "http://localhost:3002"
+export const serverUrl = "https://kitab-s7zy.vercel.app"
